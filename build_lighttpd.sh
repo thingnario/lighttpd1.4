ebb7760ee4d18f91e2eb05c9190c7f4d1f8ff8aa
eric11220 <b99902080@ntu.edu.tw>
2018-03-12 15:42:38 +0800

    Ubuntu local build

--- build_lighttpd.sh
+++ build_lighttpd.sh
@@ -18,16 +18,27 @@ item="${ADDR[0]}"
 ARCH=`echo $item | sed -e 's/-gcc.*//g'`
 
 # ======== lighttpd with static build ========
-export ARCH=$ARCH
-export AR=${ARCH}-ar
-export AS=${ARCH}-as
-export LD=${ARCH}-ld
-export RANLIB=${ARCH}-ranlib
-export CC=${ARCH}-gcc
-export NM=${ARCH}-nm
-
 ./autogen.sh
-LIGHTTPD_STATIC=yes ./configure --prefix=$tool_chain_path --target=${ARCH} --host=${ARCH} --enable-static=yes --without-bzip2 --without-pcre --disable-ipv6 --with-zlib=$tool_chain_path/lib
+
+export ARCH=$ARCH
+if [ "$ARCH" == "" ]; then
+	export AR=ar
+	export AS=as
+	export LD=ld
+	export RANLIB=ranlib
+	export CC=gcc
+	export NM=nm
+	LIGHTTPD_STATIC=yes ./configure --prefix=$tool_chain_path --enable-static=yes --without-bzip2 --without-pcre --disable-ipv6 --with-zlib=$tool_chain_path/lib
+else
+	export AR=${ARCH}-ar
+	export AS=${ARCH}-as
+	export LD=${ARCH}-ld
+	export RANLIB=${ARCH}-ranlib
+	export CC=${ARCH}-gcc
+	export NM=${ARCH}-nm
+	LIGHTTPD_STATIC=yes ./configure --prefix=$tool_chain_path --target=${ARCH} --host=${ARCH} --enable-static=yes --without-bzip2 --without-pcre --disable-ipv6 --with-zlib=$tool_chain_path/lib
+fi
+
 make clean
 make
 sudo "PATH=$PATH" make install
