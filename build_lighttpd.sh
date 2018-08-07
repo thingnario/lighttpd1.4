136589e8c557c04554b8b06780f549c0474791b5
eric11220 <b99902080@ntu.edu.tw>
2018-08-07 08:19:34 +0000

    Take one more argument as installation dir

--- build_lighttpd.sh
+++ build_lighttpd.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-if [ $# -ne 1 ]; then
-    echo $0: usage: cross_compile_library.sh ARCH 
-    echo "example: usage: cross_compile_library.sh [ arm-linux | arm-linux-gnueabihf | arm-linux-gnueabi ]"
-    exit 1
+if [ "$#" -ne 2 ]; then
+    echo "Usage: ./build_lighttpd.sh tool_chain_path install_path!"
+    echo "Example: ./build_lighttpd.sh /usr/local/arm-linux /Desktop/eric/logger/build/moxa-ia240/lighttpd"
+    exit
 fi
 
 export PATH="$PATH:$1/bin"
 
 tool_chain_path=$1
+install_path=$2/../
 #ARCH=`echo $1 | awk -F"/" '{print (NF>1)? $NF : $1}'`
 
 # linux architecture 
@@ -28,7 +29,7 @@ if [ "$ARCH" == "" ]; then
 	export RANLIB=ranlib
 	export CC=gcc
 	export NM=nm
-	LIGHTTPD_STATIC=yes ./configure --prefix=$tool_chain_path --enable-static=yes --without-bzip2 --without-pcre --disable-ipv6 --with-zlib=$tool_chain_path/lib
+	LIGHTTPD_STATIC=yes ./configure --prefix=$2 --enable-static=yes --without-bzip2 --without-pcre --disable-ipv6 --with-zlib=$install_path/zlib/lib
 else
 	export AR=${ARCH}-ar
 	export AS=${ARCH}-as
@@ -36,9 +37,9 @@ else
 	export RANLIB=${ARCH}-ranlib
 	export CC=${ARCH}-gcc
 	export NM=${ARCH}-nm
-	LIGHTTPD_STATIC=yes ./configure --prefix=$tool_chain_path --target=${ARCH} --host=${ARCH} --enable-static=yes --without-bzip2 --without-pcre --disable-ipv6 --with-zlib=$tool_chain_path/lib
+	LIGHTTPD_STATIC=yes ./configure --prefix=$2 --target=${ARCH} --host=${ARCH} --enable-static=yes --without-bzip2 --without-pcre --disable-ipv6 --with-zlib=$install_path/zlib/lib
 fi
 
 make clean
 make
-sudo "PATH=$PATH" make install
+make install
